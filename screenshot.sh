66272c13022c47ffb0e133d4663301393373b797
Ruvenss G. Wilches <ruvenss@gmail.com>
2021-09-10 18:16:54 +0200

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-uploadserver=https://YOUR_SERVER/
+uploadserver=https://upload.nizu.io/
+myuser=$(whoami)
 user=1
-key=YOUR_KEY
-pngfile=$(date +"%Y-%m-%d-%H-%M").png
-webfile=$(date +"%Y-%m-%d-%H-%M").webp
-curlfile='files[]=@.screenshots-bg/'
-curlf="curl -F '${curlfile}${webfile}' -F 'key=${key}' ${uploadserver}"
+key=zFaZdHVYiseUsRgmqnm9MerIPBC4T1Re
+curlfile="files[]=@/home/$myuser/.screenshots-bg/"
 mkdir -p ~/.screenshots-bg
-scrot -z -u '%Y-%m-%d-%H-%M.png' -d1 -e 'mv $f ~/.screenshots-bg/'
-convert ~/.screenshots-bg/$pngfile -quality 30% ~/.screenshots-bg/$webfile
-rm ~/.screenshots-bg/$pngfile
-eval $curlf
+#import -window root ~/.screenshots-bg/$pngfile
+while true; do
+	pngfile=$(date +"%Y-%m-%d-%H-%M").png
+	webfile=$(date +"%Y-%m-%d-%H-%M").webp
+	curlf="curl -F '${curlfile}${webfile}' -F 'key=${key}' ${uploadserver}"
+	scrot -z -u '%Y-%m-%d-%H-%M.png' -e 'mv $f ~/.screenshots-bg/'
+	convert ~/.screenshots-bg/$pngfile -quality 30% ~/.screenshots-bg/$webfile
+	rm ~/.screenshots-bg/$pngfile
+	eval $curlf
+	#echo $curlf | /bin/bash
+	rm ~/.screenshots-bg/$webfile
+	echo "___"
+	echo "$myuser upload file $webfile to $uploadserver using command"
+	echo "$curlf"
+	echo ""
+	sleep 60
+done
